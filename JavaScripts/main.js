61f3668b94edbd868a3e38373c91c2c7dc081692
MehemmedBagirov56 <bgrvmehemmed056@gmail.com>
2026-01-27 19:02:13 +0400

    JavaScriptttt

---
+++ JavaScripts/main.js
@@ -0,0 +1,62 @@
+// TASK 1
+// console.log("My name is Maqa");
+
+
+// TASK 2
+// const name = "Maqa";
+// console.log (name);
+
+// Task 3
+// person 
+// &add
+
+// Task 4
+// let bookprice = 19;
+// let bookAmount = 5;
+// console.log(bookprice * bookAmount);
+
+// task 5
+// let kmhSpeed = 90;
+// let msSpeed = (kmhSpeed * 1000) / 3600;
+
+// console.log(msSpeed);
+
+// task 6
+// let birthDate = new Date(prompt("Doğum tarixini daxil et (YYYY-MM-DD):"));
+
+// console.log(birthDate);
+
+// task 7
+// let num = 10;
+// console.log(num ** 5);
+// console.log(num % 3);
+
+
+// task 8 +9  (ikisi birlikdedir :)
+// let firstName = "Maqa";
+// let lastName = "Bagirov";
+// console.log(`Sizin adınız ${firstName} ${lastName}. Tanışlığımıza çox şadam!`);
+
+
+// task 10
+// let fullName = "Maqa Bagirov";
+// console.log(fullName.length);
+
+// task 11
+// let fullName = "Maqa Bagirov";
+// console.log(fullName [0]);
+// console.log(fullName [5]);
+
+// task 12
+// let Name = prompt("Adınızı daxil edin:");
+// let lastName = prompt("Soyadınızı daxil edin:");
+
+// console.log(`${lastName} ${Name}`);
+
+// task 13
+// let fullName = prompt("Adınızı daxil edin:");
+// console.log(`Salam, ${fullName}!`)
+
+// task 14
+
+
