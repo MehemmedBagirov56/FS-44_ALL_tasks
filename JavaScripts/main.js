f3a36ed5aa62a7ce602e52fa788545c7790e8dcf
MehemmedBagirov56 <bgrvmehemmed056@gmail.com>
2026-01-29 16:02:08 +0400

    JF YENI TASKLARR

--- JavaScripts/main.js
+++ JavaScripts/main.js
@@ -58,5 +58,82 @@
 // console.log(`Salam, ${fullName}!`)
 
 // task 14
+// let a = Number(prompt("Reqem daxil edin"));
+// let b = Number(prompt("ikinci reqemi daxil edin"));
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+// task 15
+// let result = 12345 * 98765;
+// let number = result.toString()[6];
+
+// console.log(number);
+
+
+// -----------------ANLAMADIMMMMMMMM-----------
+// task 16
+// task 17
+
+
+
+
+// task 18
+// let fullName = prompt("Ad ve soyadini daxil ele");
+
+// console.log(`Salam, ${fullName}!`);
+
+
+// task 19
+// let price = prompt("Qiymət daxil edinn (530.90)");
+// let parts = price.split(".");
+
+// console.log(`${parts[0]} manat ${parts[1]} qəpik`);
+
+
+// task 20
+// let number = prompt("Maasini qeyd ele emioglu");
+// let number2 = (number * 100) / 13 ;
+
+// console.log(number2);
+
+// task 21
+
+// let fullName = prompt("Soyad Ad Ata adıni daxil edin","Ivan Pyotor Sergevic");
+
+// let parts = fullName.split(" "); 
+// let lastName = parts[0];
+// let firstName = parts[1];
+// let fatherName = parts[2];
+
+// console.log(`${lastName} ${firstName[0]}.${fatherName[0]}.`);
+
+// task 22
+
+// let amount = (prompt("Məbləğ"));
+// let years = (prompt("Müddət (il)"));
+// let percent =(prompt("Faiz"));
+
+// let profit = (amount * percent) / (100 * years);
+// console.log(profit);
+
+// task 23
+
+// let value = 20
+
+// console.log(value === 10);
+// console.log(value < 12);
+// console.log(value <= 10);
+// console.log(value !== 20);
+// // ----BU SONUNCUSU ALINMIRRRRRRRRRRRR------- :)
+// console.log(value !== 11 );     
+
+
+
+
+
+
 
 
